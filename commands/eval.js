0805716d34593edeb668d99414202195d73cba59
PaperDoom <36348714+PaperDoom@users.noreply.github.com>
2019-02-12 11:12:32 -0600

    lsdjfs

--- commands/eval.js
+++ commands/eval.js
@@ -2,28 +2,24 @@
 
 exports.run = async (client, message, args) => {
 
-    let cleaner = async function(client, text) {
-        if (text && text.constructor.name == "Promise")
-          text = await text;
-        if (typeof evaled !== "string")
-          text = require("util").inspect(text, {depth: 1});
-    
-        text = text
-          .replace(/`/g, "`" + String.fromCharCode(8203))
-          .replace(/@/g, "@" + String.fromCharCode(8203))
-          .replace(client.token, "mfa.VkO_2G4Qv3T--NO--lWetW_tjND--TOKEN--QFTm6YGtzq9PH--4U--tG0");
-    
-        return text;
-      };
+    let clean = text => {
+        if (typeof(text) === "string")
+          return text.replace(/`/g, "`" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203));
+        else
+            return text;
+      }
 
-    let code = args.join(" ");
-    try {
-      let evaled = eval(code);
-      let clean = await cleaner(client, evaled);
-      message.channel.send(`\`\`\`js\n${clean}\n\`\`\``);
-    } catch (err) {
-      message.channel.send(`\`ERROR\` \`\`\`xl\n${await cleaner(client, err)}\n\`\`\``);
-    }
+      try {
+        let code = args.join(" ");
+        let evaled = eval(code);
+   
+        if (typeof evaled !== "string")
+          evaled = require("util").inspect(evaled);
+   
+        message.channel.send(clean(evaled), {code:"xl"});
+      } catch (err) {
+        message.channel.send(`\`ERROR\` \`\`\`xl\n${clean(err)}\n\`\`\``);
+      }
 
 };
 
